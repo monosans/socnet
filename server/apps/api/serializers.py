977ee8559099654a4ec1c4a6f6ff2a6141b35806
monosans <hsyqixco@protonmail.com>
2022-06-15 20:38:02 +0300

    Reconfigure mypy

--- server/apps/api/serializers.py
+++ server/apps/api/serializers.py
@@ -18,26 +18,36 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    chats = serializers.HyperlinkedRelatedField(
+    chats = serializers.HyperlinkedRelatedField(  # type: ignore[var-annotated]
         many=True, read_only=True, view_name="chat-detail"
     )
-    liked_comments = serializers.HyperlinkedRelatedField(
-        many=True, read_only=True, view_name="postcomment-detail"
+    liked_comments = (
+        serializers.HyperlinkedRelatedField(  # type: ignore[var-annotated]
+            many=True, read_only=True, view_name="postcomment-detail"
+        )
     )
-    liked_posts = serializers.HyperlinkedRelatedField(
+    liked_posts = (
+        serializers.HyperlinkedRelatedField(  # type: ignore[var-annotated]
+            many=True, read_only=True, view_name="post-detail"
+        )
+    )
+    outgoing_messages = (
+        serializers.HyperlinkedRelatedField(  # type: ignore[var-annotated]
+            many=True, read_only=True, view_name="message-detail"
+        )
+    )
+    post_comments = (
+        serializers.HyperlinkedRelatedField(  # type: ignore[var-annotated]
+            many=True, read_only=True, view_name="postcomment-detail"
+        )
+    )
+    posts = serializers.HyperlinkedRelatedField(  # type: ignore[var-annotated]
         many=True, read_only=True, view_name="post-detail"
     )
-    outgoing_messages = serializers.HyperlinkedRelatedField(
-        many=True, read_only=True, view_name="message-detail"
-    )
-    post_comments = serializers.HyperlinkedRelatedField(
-        many=True, read_only=True, view_name="postcomment-detail"
-    )
-    posts = serializers.HyperlinkedRelatedField(
-        many=True, read_only=True, view_name="post-detail"
-    )
-    subscribers = serializers.HyperlinkedRelatedField(
-        many=True, read_only=True, view_name="subscriber-detail"
+    subscribers = (
+        serializers.HyperlinkedRelatedField(  # type: ignore[var-annotated]
+            many=True, read_only=True, view_name="subscriber-detail"
+        )
     )
 
     class Meta:
@@ -56,8 +66,10 @@ class PostCommentSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class PostSerializer(serializers.HyperlinkedModelSerializer):
-    comments = serializers.HyperlinkedRelatedField(
-        many=True, read_only=True, view_name="postcomment-detail"
+    comments = (
+        serializers.HyperlinkedRelatedField(  # type: ignore[var-annotated]
+            many=True, read_only=True, view_name="postcomment-detail"
+        )
     )
 
     class Meta:
@@ -72,8 +84,10 @@ class MessageSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class ChatSerializer(serializers.HyperlinkedModelSerializer):
-    messages = serializers.HyperlinkedRelatedField(
-        many=True, read_only=True, view_name="message-detail"
+    messages = (
+        serializers.HyperlinkedRelatedField(  # type: ignore[var-annotated]
+            many=True, read_only=True, view_name="message-detail"
+        )
     )
 
     class Meta:
