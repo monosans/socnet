8e87f9921ec17586efa5614d58c41dec68fdfe1b
monosans <hsyqixco@protonmail.com>
2022-06-23 13:33:01 +0300

    Update asgi.py

--- server/asgi.py
+++ server/asgi.py
@@ -1,19 +1,22 @@
 """https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/"""
+"""https://channels.readthedocs.io/en/latest/introduction.html#django-integration"""
 import os
 
-import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
-django.setup()
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
 from .apps.messenger.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": AllowedHostsOriginValidator(
             AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
         ),
