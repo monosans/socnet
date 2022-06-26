e36e929278088143d31f0135835a2a706cb02d6c
monosans <hsyqixco@protonmail.com>
2022-06-26 15:23:29 +0300

    Edit Dockerfile and entrypoint.sh

--- docker/django/entrypoint.sh
+++ docker/django/entrypoint.sh
@@ -8,23 +8,15 @@ readonly cmd="$*"
 
 : "${DJANGO_DATABASE_HOST:=db}"
 : "${DJANGO_DATABASE_PORT:=5432}"
+: "${DJANGO_REDIS_HOST:=redis}"
+: "${DJANGO_REDIS_PORT:=6379}"
 
 # Make sure that this container is started
 # after the ones with Postgres and Redis:
 dockerize \
   -wait "tcp://${DJANGO_DATABASE_HOST}:${DJANGO_DATABASE_PORT}" \
-  -timeout 90s
-
->&2 echo 'Postgres is up - continuing...'
-
-: "${DJANGO_REDIS_HOST:=redis}"
-: "${DJANGO_REDIS_PORT:=6379}"
-
-dockerize \
   -wait "tcp://${DJANGO_REDIS_HOST}:${DJANGO_REDIS_PORT}" \
   -timeout 90s
-
->&2 echo 'Redis is up - continuing...'
 
 # Evaluating passed command
 exec $cmd
