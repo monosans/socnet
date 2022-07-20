f6a2c039794ba205a21bb84f255aa86413f9c902
monosans <hsyqixco@protonmail.com>
2022-07-20 20:03:29 +0300

    Precompress static files with Brotli and gzip

--- docker/django/start.sh
+++ docker/django/start.sh
@@ -8,4 +8,9 @@ python3 /app/manage.py migrate --noinput
 python3 /app/manage.py collectstatic --noinput --clear
 python3 /app/manage.py compilemessages
 
+find /var/www/django/static -type f \
+	! -regex '^.+\.\(jpg\|jpeg\|png\|gif\|webp\|zip\|gz\|tgz\|bz2\|tbz\|xz\|br\|swf\|flv\|woff\|woff2\)$' \
+	-exec brotli --force --best {} \+ \
+	-exec gzip --force --keep --best {} \+
+
 /usr/local/bin/gunicorn --config python:docker.django.gunicorn_config server.asgi:application
