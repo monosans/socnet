11f5c0ea94b0af676ef1deb593a28855033b61e9
monosans <hsyqixco@protonmail.com>
2022-07-29 23:53:06 +0300

    Disable max_requests in gunicorn

--- docker/django/gunicorn_config.py
+++ docker/django/gunicorn_config.py
@@ -7,8 +7,8 @@ bind = "0.0.0.0:5000"
 workers = multiprocessing.cpu_count() * 2 + 1
 
 # https://adamj.eu/tech/2019/09/19/working-around-memory-leaks-in-your-django-app/
-max_requests = 1000
-max_requests_jitter = 100
+# max_requests = 1000
+# max_requests_jitter = 100
 
 chdir = "/app"
 worker_tmp_dir = "/dev/shm"
