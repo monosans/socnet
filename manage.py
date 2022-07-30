ee645698cddc59472e408e278d9297195be0d4d0
monosans <hsyqixco@protonmail.com>
2022-07-30 15:48:15 +0300

    Remove try/except from manage.py

--- manage.py
+++ manage.py
@@ -7,14 +7,8 @@ import sys
 
 def main() -> None:
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            + "available on your PYTHONPATH environment variable? Did you "
-            + "forget to activate a virtual environment?"
-        ) from exc
+    from django.core.management import execute_from_command_line
+
     execute_from_command_line(sys.argv)
 
 
