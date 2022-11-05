f0b004045c9473a27102c65a54c8fc77861e29bd
monosans <hsyqixco@protonmail.com>
2022-11-05 14:08:41 +0300

    Use custom exceptions in signals

--- socnet/messenger/signals.py
+++ socnet/messenger/signals.py
@@ -2,20 +2,18 @@ from __future__ import annotations
 
 from typing import Any
 
-from django.core.exceptions import ValidationError
 from django.db.models.signals import m2m_changed
 from django.utils.translation import gettext as _
 
-from . import models
+from . import exceptions, models
 
 
 def validate_chat_participants_count(
     instance: models.Chat, action: str, **kwargs: Any
 ) -> None:
     if action == "post_add" and instance.participants.count() != 2:
-        raise ValidationError(
-            _("The chat must have 2 participants."),
-            code="invalid_chat_participants_count",
+        raise exceptions.ChatParticipantsCountError(
+            _("The chat must have 2 participants.")
         )
 
 
--- socnet/users/signals.py
+++ socnet/users/signals.py
@@ -3,10 +3,10 @@ from __future__ import annotations
 from typing import Any, Set
 
 from django.contrib.auth import get_user_model
-from django.core.exceptions import ValidationError
 from django.db.models.signals import m2m_changed
 from django.utils.translation import gettext as _
 
+from . import exceptions
 from .models import User as UserType
 
 User = get_user_model()
@@ -16,8 +16,8 @@ def forbid_self_subscription(
     instance: UserType, action: str, pk_set: Set[int], **kwargs: Any
 ) -> None:
     if action == "pre_add" and instance.pk in pk_set:
-        raise ValidationError(
-            _("You can't subscribe to yourself."), code="self_subscription"
+        raise exceptions.SelfSubscriptionError(
+            _("You can't subscribe to yourself.")
         )
 
 
