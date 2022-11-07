b97a89eaf4a135083e0cac10065b9810af2bbabe
monosans <hsyqixco@protonmail.com>
2022-11-07 21:24:58 +0300

    Add "Admin page" button to users, posts and comments

---
+++ socnet/core/templatetags/obj_admin_url.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from django import template
+from django.contrib.admin.templatetags.admin_urls import admin_urlname
+from django.db.models import Model
+from django.urls import reverse
+from django.utils.safestring import SafeString
+
+register = template.Library()
+
+
+@register.filter()
+def obj_admin_url(obj: Model) -> str:
+    urlname = admin_urlname(obj._meta, SafeString("change"))
+    return reverse(urlname, args=(obj.pk,))
