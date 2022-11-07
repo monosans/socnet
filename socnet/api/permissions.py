7dd060cf296c1dbee96753fd969b2d26ae8738d2
monosans <hsyqixco@protonmail.com>
2022-11-08 00:07:10 +0300

    Require view permission to be able to send HEAD requests to viewsets

--- socnet/api/permissions.py
+++ socnet/api/permissions.py
@@ -6,6 +6,6 @@ from rest_framework.permissions import DjangoModelPermissions
 
 
 class DjangoModelPermissionsWithViewPermissionCheck(DjangoModelPermissions):
-    def __init__(self) -> None:
-        self.perms_map = deepcopy(self.perms_map)
-        self.perms_map["GET"] = ["%(app_label)s.view_%(model_name)s"]
+    perms_map = deepcopy(DjangoModelPermissions.perms_map)
+    perms_map["GET"] = ["%(app_label)s.view_%(model_name)s"]
+    perms_map["HEAD"] = ["%(app_label)s.view_%(model_name)s"]
