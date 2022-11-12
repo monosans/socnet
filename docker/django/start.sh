43bc1a85f080196dd5c686597fe5592db3ed811e
monosans <hsyqixco@protonmail.com>
2022-11-12 09:59:04 +0300

    Ignore video files brotli/gzip compression

--- docker/django/start.sh
+++ docker/django/start.sh
@@ -7,7 +7,7 @@ python3 /app/manage.py collectstatic --noinput --clear
 python3 /app/manage.py compilemessages
 
 find /var/www/django/static -type f \
-	! -regex '^.+\.\(jpg\|jpeg\|png\|gif\|webp\|zip\|gz\|tgz\|bz2\|tbz\|xz\|br\|swf\|flv\|woff\|woff2\)$' \
+	! -regex '^.+\.\(jpg\|jpeg\|png\|gif\|webp\|zip\|gz\|tgz\|bz2\|tbz\|xz\|br\|swf\|flv\|woff\|woff2\|3gp\|3gpp\|asf\|avi\|m4v\|mov\|mp4\|mpeg\|mpg\|webm\|wmv\)$' \
 	-exec brotli --force --best {} \+ \
 	-exec gzip --force --keep --best {} \+
 
