1ad7ca42d47f94b2d53ff8677c175004337df37e
monosans <hsyqixco@protonmail.com>
2022-11-22 22:27:28 +0300

    Use contextlib.contextmanager for AssertCountDiff

--- tests/utils.py
+++ tests/utils.py
@@ -1,28 +1,17 @@
 from __future__ import annotations
 
-from types import TracebackType
-from typing import Optional, Type
+from contextlib import contextmanager
+from typing import Generator, Type
 
 from django.db.models import Model
 
 
-class AssertCountDiff:
-    __slots__ = ("model", "diff", "enter_count")
-
-    def __init__(self, model: Type[Model], diff: int) -> None:
-        self.model = model
-        self.diff = diff
-
-    def _get_count(self) -> int:
-        return self.model.objects.count()
-
-    def __enter__(self) -> None:
-        self.enter_count = self._get_count()
-
-    def __exit__(
-        self,
-        exc_type: Optional[Type[BaseException]],
-        exc_val: Optional[BaseException],
-        exc_tb: Optional[TracebackType],
-    ) -> None:
-        assert self._get_count() == self.enter_count + self.diff
+@contextmanager
+def assert_count_diff(
+    model: Type[Model], diff: int
+) -> Generator[None, None, None]:
+    enter_count = model.objects.count()
+    try:
+        yield
+    finally:
+        assert model.objects.count() == enter_count + diff
