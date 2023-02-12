dbe782b98e8d0cb2d01bd1a5c2e8591ec7b20f9a
monosans <hsyqixco@protonmail.com>
2023-02-12 11:15:33 +0300

    Remove rest framework parser and authentication settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -174,10 +174,6 @@ ACCOUNT_PRESERVE_USERNAME_CASING = False
 
 REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": ["rest_framework.renderers.JSONRenderer"],
-    "DEFAULT_PARSER_CLASSES": ["rest_framework.parsers.JSONParser"],
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthentication"
-    ],
     "DEFAULT_PERMISSION_CLASSES": [
         "socnet.api.permissions.ActualDjangoModelPermissions"
     ],
