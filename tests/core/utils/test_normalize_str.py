50213e27692e88bd988faab24c185def83a50c19
monosans <hsyqixco@protonmail.com>
2023-02-24 10:14:50 +0300

    Add normalize_str tests

---
+++ tests/core/utils/test_normalize_str.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from typing import TypeVar
+
+import pytest
+
+from socnet.core.utils import normalize_str
+
+T = TypeVar("T")
+
+
+@pytest.mark.parametrize(
+    "given,expected",
+    (
+        ("string", "string"),
+        (
+            "\n\n \tstring\t  \n\n\n   string  \tstring  \n  \t ",
+            "string\nstring string",
+        ),
+        (1, 1),
+    ),
+)
+def test_normalize_str(given: T, expected: T) -> None:
+    assert normalize_str(given) == expected
