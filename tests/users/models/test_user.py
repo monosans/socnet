f7bde5f70c5dec048a8090fc826ee700ead91c83
monosans <hsyqixco@protonmail.com>
2023-02-28 17:51:55 +0300

    Add birth_date tests

--- tests/users/models/test_user.py
+++ tests/users/models/test_user.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
 
+from datetime import timedelta
+
 import pytest
 from django.core.exceptions import ValidationError
 from django.db import IntegrityError
+from django.utils import timezone
 
 from socnet.users.models import User
 
@@ -37,3 +40,22 @@ def test_username_unique() -> None:
 def test_username_case_insensitive() -> None:
     factory(username="user")
     assert User.objects.filter(username="USER").exists()
+
+
+def test_birth_date_unrealistically_old() -> None:
+    user = factory.build(birth_date="1900-01-01")
+    with pytest.raises(ValidationError):
+        user.full_clean()
+
+
+def test_birth_date_in_future() -> None:
+    tomorrow = timezone.now() + timedelta(days=1)
+    user = factory.build(birth_date=tomorrow)
+    with pytest.raises(ValidationError):
+        user.full_clean()
+
+
+def test_birth_date_today() -> None:
+    tomorrow = timezone.now()
+    user = factory.build(birth_date=tomorrow)
+    user.full_clean()
