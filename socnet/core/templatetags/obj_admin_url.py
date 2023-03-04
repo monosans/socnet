f6f3ef1965039c084fbea790b7fba62956744717
monosans <hsyqixco@protonmail.com>
2023-03-05 00:19:35 +0300

    Use mark_safe instead of SafeString

--- socnet/core/templatetags/obj_admin_url.py
+++ socnet/core/templatetags/obj_admin_url.py
@@ -4,12 +4,12 @@ from django import template
 from django.contrib.admin.templatetags.admin_urls import admin_urlname
 from django.db.models import Model
 from django.urls import reverse
-from django.utils.safestring import SafeString
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 
 @register.filter()
 def obj_admin_url(obj: Model) -> str:
-    urlname = admin_urlname(obj._meta, SafeString("change"))  # noqa: SLF001
+    urlname = admin_urlname(obj._meta, mark_safe("change"))  # noqa: SLF001
     return reverse(urlname, args=(obj.pk,))
