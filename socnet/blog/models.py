f212b330427ce50721e4906c706ae79c54084dac
monosans <hsyqixco@protonmail.com>
2023-03-05 12:56:28 +0300

    Fix CI

--- socnet/blog/models.py
+++ socnet/blog/models.py
@@ -1,10 +1,9 @@
 from __future__ import annotations
 
 from django.conf import settings
-from django.core.exceptions import ValidationError
 from django.db import models
 from django.urls import reverse
-from django.utils.translation import gettext, gettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class Post(models.Model):
