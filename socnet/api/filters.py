a434230955909b51afed9db44ad7dbda1827a191
monosans <hsyqixco@protonmail.com>
2023-03-06 16:43:44 +0300

    Simplify filters fields syntax

--- socnet/api/filters.py
+++ socnet/api/filters.py
@@ -46,112 +46,110 @@ BASE_FIELDS = {"id": ID_FIELDS}
 class ChatFilter(FilterSet):
     class Meta:
         model = messenger_models.Chat
-        fields = {**BASE_FIELDS}
+        fields = BASE_FIELDS.copy()
 
 
 class ContentTypeFilter(FilterSet):
     class Meta:
         model = ContentType
-        fields = {
-            **BASE_FIELDS,
-            "app_label": LOWERCASE_CHAR_FIELDS,
-            "model": LOWERCASE_CHAR_FIELDS,
-        }
+        fields = dict(
+            BASE_FIELDS, app_label=LOWERCASE_CHAR_FIELDS, model=LOWERCASE_CHAR_FIELDS
+        )
 
 
 class EmailAddressFilter(FilterSet):
     class Meta:
         model = EmailAddress
-        fields = {
-            **BASE_FIELDS,
-            "email": LOWERCASE_CHAR_FIELDS,
-            "verified": BOOL_FIELDS,
-            "primary": BOOL_FIELDS,
-            "user": ID_FIELDS,
-        }
+        fields = dict(
+            BASE_FIELDS,
+            email=LOWERCASE_CHAR_FIELDS,
+            verified=BOOL_FIELDS,
+            primary=BOOL_FIELDS,
+            user=ID_FIELDS,
+        )
 
 
 class GroupFilter(FilterSet):
     class Meta:
         model = auth_models.Group
-        fields = {**BASE_FIELDS, "name": CHAR_FIELDS}
+        fields = dict(BASE_FIELDS, name=CHAR_FIELDS)
 
 
 class LogEntryFilter(FilterSet):
     class Meta:
         model = LogEntry
-        fields = {
-            **BASE_FIELDS,
-            "action_time": DATETIME_FIELDS,
-            "object_id": [*ID_FIELDS, *NULLABLE],
-            "object_repr": CHAR_FIELDS,
-            "action_flag": ID_FIELDS,
-            "change_message": CHAR_FIELDS,
-            "user": ID_FIELDS,
-            "content_type": [*ID_FIELDS, *NULLABLE],
-        }
+        fields = dict(
+            BASE_FIELDS,
+            action_time=DATETIME_FIELDS,
+            object_id=[*ID_FIELDS, *NULLABLE],
+            object_repr=CHAR_FIELDS,
+            action_flag=ID_FIELDS,
+            change_message=CHAR_FIELDS,
+            user=ID_FIELDS,
+            content_type=[*ID_FIELDS, *NULLABLE],
+        )
 
 
 class MessageFilter(FilterSet):
     class Meta:
         model = messenger_models.Message
-        fields = {
-            **BASE_FIELDS,
-            "content": CHAR_FIELDS,
-            "date_created": DATETIME_FIELDS,
-            "sender": ID_FIELDS,
-            "chat": ID_FIELDS,
-        }
+        fields = dict(
+            BASE_FIELDS,
+            content=CHAR_FIELDS,
+            date_created=DATETIME_FIELDS,
+            sender=ID_FIELDS,
+            chat=ID_FIELDS,
+        )
 
 
 class PermissionFilter(FilterSet):
     class Meta:
         model = auth_models.Permission
-        fields = {
-            **BASE_FIELDS,
-            "name": CHAR_FIELDS,
-            "codename": LOWERCASE_CHAR_FIELDS,
-            "content_type": ID_FIELDS,
-        }
+        fields = dict(
+            BASE_FIELDS,
+            name=CHAR_FIELDS,
+            codename=LOWERCASE_CHAR_FIELDS,
+            content_type=ID_FIELDS,
+        )
 
 
 class PostCommentFilter(FilterSet):
     class Meta:
         model = blog_models.PostComment
-        fields = {
-            **BASE_FIELDS,
-            "content": CHAR_FIELDS,
-            "date_created": DATETIME_FIELDS,
-            "date_updated": DATETIME_FIELDS,
-            "post": ID_FIELDS,
-            "author": ID_FIELDS,
-        }
+        fields = dict(
+            BASE_FIELDS,
+            content=CHAR_FIELDS,
+            date_created=DATETIME_FIELDS,
+            date_updated=DATETIME_FIELDS,
+            post=ID_FIELDS,
+            author=ID_FIELDS,
+        )
 
 
 class PostFilter(FilterSet):
     class Meta:
         model = blog_models.Post
-        fields = {
-            **BASE_FIELDS,
-            "content": CHAR_FIELDS,
-            "date_created": DATETIME_FIELDS,
-            "date_updated": DATETIME_FIELDS,
-            "author": ID_FIELDS,
-        }
+        fields = dict(
+            BASE_FIELDS,
+            content=CHAR_FIELDS,
+            date_created=DATETIME_FIELDS,
+            date_updated=DATETIME_FIELDS,
+            author=ID_FIELDS,
+        )
 
 
 class UserFilter(FilterSet):
     class Meta:
         model = User
-        fields = {
-            **BASE_FIELDS,
-            "is_superuser": BOOL_FIELDS,
-            "is_staff": BOOL_FIELDS,
-            "is_active": BOOL_FIELDS,
-            "username": LOWERCASE_CHAR_FIELDS,
-            "display_name": CHAR_FIELDS,
-            "email": LOWERCASE_CHAR_FIELDS,
-            "birth_date": [*DATE_FIELDS, *NULLABLE],
-            "location": CHAR_FIELDS,
-            "about": CHAR_FIELDS,
-        }
+        fields = dict(
+            BASE_FIELDS,
+            is_superuser=BOOL_FIELDS,
+            is_staff=BOOL_FIELDS,
+            is_active=BOOL_FIELDS,
+            username=LOWERCASE_CHAR_FIELDS,
+            display_name=CHAR_FIELDS,
+            email=LOWERCASE_CHAR_FIELDS,
+            birth_date=[*DATE_FIELDS, *NULLABLE],
+            location=CHAR_FIELDS,
+            about=CHAR_FIELDS,
+        )
