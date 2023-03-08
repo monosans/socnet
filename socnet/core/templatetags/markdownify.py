384cbb098fa60e32b948d3d2f3e47a2da5a288e8
monosans <hsyqixco@protonmail.com>
2023-03-08 12:36:51 +0300

    Replace bootstrapify.js with server-side lxml

--- socnet/core/templatetags/markdownify.py
+++ socnet/core/templatetags/markdownify.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import lxml.etree
+import lxml.html
 import nh3
 from django import template
 from django.utils.safestring import SafeString, mark_safe
@@ -14,6 +16,21 @@ md = Markdown(
 MARKDOWN_HELP_TEXT = _("Supports a safe subset of HTML and Markdown.")
 
 
+def bootstrapify(tree: lxml.etree._Element) -> None:  # noqa: SLF001
+    for table in tree.iter("table"):
+        table.attrib["class"] = "table"
+
+
+def lazy_img(tree: lxml.etree._Element) -> None:  # noqa: SLF001
+    for img in tree.iter("img"):
+        img.attrib["loading"] = "lazy"
+
+
 @register.filter()
 def markdownify(value: str) -> SafeString:
-    return mark_safe(nh3.clean(md.convert(value)))
+    sanitized_html = nh3.clean(md.convert(value))
+    tree = lxml.html.fromstring(sanitized_html)
+    bootstrapify(tree)
+    lazy_img(tree)
+    tree_str = lxml.etree.tostring(tree, encoding=str, method="html")
+    return mark_safe(tree_str)
--- socnet/static/js/bootstrapify.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-(() => {
-  const tables = document.getElementsByTagName("table");
-  for (const table of tables) {
-    table.classList.add("table");
-  }
-})();
