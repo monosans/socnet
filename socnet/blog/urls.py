b274734ae16ff513df1b866099d4d6e5fd3160dc
monosans <hsyqixco@protonmail.com>
2023-03-16 19:46:31 +0300

    Make blog urls more readable

--- socnet/blog/urls.py
+++ socnet/blog/urls.py
@@ -1,31 +1,31 @@
 from __future__ import annotations
 
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
 app_name = "blog"
+
+post_patterns = [
+    path("<int:pk>/", views.post_view, name="post"),
+    path("create/", views.PostCreateView.as_view(), name="post_create"),
+    path("<int:pk>/edit/", views.PostUpdateView.as_view(), name="post_update"),
+    path("<int:pk>/delete/", views.post_delete_view, name="post_delete"),
+]
+comment_patterns = [
+    path("edit/", views.CommentUpdateView.as_view(), name="comment_update"),
+    path("delete/", views.comment_delete_view, name="comment_delete"),
+]
+user_patterns = [
+    path("", views.user_view, name="user"),
+    path("posts/", views.user_posts_view, name="user_posts"),
+    path("liked/", views.liked_posts_view, name="liked_posts"),
+    path("subscribers/", views.subscribers_view, name="subscribers"),
+    path("subscriptions/", views.subscriptions_view, name="subscriptions"),
+]
 urlpatterns = [
     path("posts/", views.posts_view, name="posts"),
-    path("post/<int:pk>/", views.post_view, name="post"),
-    path("post/create/", views.PostCreateView.as_view(), name="post_create"),
-    path("post/<int:pk>/edit/", views.PostUpdateView.as_view(), name="post_update"),
-    path("post/<int:pk>/delete/", views.post_delete_view, name="post_delete"),
-    path(
-        "comment/<int:pk>/edit/",
-        views.CommentUpdateView.as_view(),
-        name="comment_update",
-    ),
-    path("comment/<int:pk>/delete/", views.comment_delete_view, name="comment_delete"),
-    path("user/<slug:username>/", views.user_view, name="user"),
-    path("user/<slug:username>/posts/", views.user_posts_view, name="user_posts"),
-    path("user/<slug:username>/liked/", views.liked_posts_view, name="liked_posts"),
-    path(
-        "user/<slug:username>/subscribers/", views.subscribers_view, name="subscribers"
-    ),
-    path(
-        "user/<slug:username>/subscriptions/",
-        views.subscriptions_view,
-        name="subscriptions",
-    ),
+    path("post/", include(post_patterns)),
+    path("comment/<int:pk>/", include(comment_patterns)),
+    path("user/<slug:username>/", include(user_patterns)),
 ]
