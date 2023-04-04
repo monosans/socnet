5961ee1199d8c0ca3001b68e0cde154cdd377754
monosans <hsyqixco@protonmail.com>
2023-04-04 22:45:36 +0300

    Configure debug toolbar

--- config/settings/local.py
+++ config/settings/local.py
@@ -21,6 +21,23 @@ INTERNAL_IPS = [
     for ip in socket.gethostbyname_ex(socket.gethostname())[2]
 ] + ["127.0.0.1", "10.0.2.2"]
 
+DEBUG_TOOLBAR_CONFIG = {
+    "DISABLE_PANELS": {
+        "debug_toolbar.panels.history.HistoryPanel",
+        "debug_toolbar.panels.versions.VersionsPanel",
+        "debug_toolbar.panels.settings.SettingsPanel",
+        "debug_toolbar.panels.headers.HeadersPanel",
+        "debug_toolbar.panels.request.RequestPanel",
+        "debug_toolbar.panels.staticfiles.StaticFilesPanel",
+        "debug_toolbar.panels.templates.TemplatesPanel",
+        "debug_toolbar.panels.cache.CachePanel",
+        "debug_toolbar.panels.signals.SignalsPanel",
+        "debug_toolbar.panels.redirects.RedirectsPanel",
+        "debug_toolbar.panels.profiling.ProfilingPanel",
+    },
+    "ENABLE_STACKTRACES": False,
+}
+
 MEDIA_ROOT = str(APPS_DIR / "media")
 
 EXTRA_CHECKS = {
