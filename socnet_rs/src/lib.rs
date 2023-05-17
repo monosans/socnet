9a2adb696b6de7af7e9a4a984271ab049c2774ed
monosans <hsyqixco@protonmail.com>
2023-05-17 23:07:35 +0300

    Reformat lib.rs

--- socnet_rs/src/lib.rs
+++ socnet_rs/src/lib.rs
@@ -1,8 +1,8 @@
+use once_cell::sync::Lazy;
+use pyo3::prelude::*;
 use std::collections::HashMap;
 
-use pyo3::prelude::*;
-
-static AMMONIA: once_cell::sync::Lazy<ammonia::Builder> = once_cell::sync::Lazy::new(|| {
+static AMMONIA: Lazy<ammonia::Builder> = Lazy::new(|| {
     let mut cleaner = ammonia::Builder::default();
     cleaner.set_tag_attribute_values(HashMap::from([
         ("img", HashMap::from([("loading", "lazy")])),
@@ -11,20 +11,19 @@ static AMMONIA: once_cell::sync::Lazy<ammonia::Builder> = once_cell::sync::Lazy:
     cleaner
 });
 
-static CMARK_OPTIONS: once_cell::sync::Lazy<pulldown_cmark::Options> =
-    once_cell::sync::Lazy::new(|| {
-        let mut options = pulldown_cmark::Options::empty();
-        options.insert(pulldown_cmark::Options::ENABLE_TABLES);
-        options.insert(pulldown_cmark::Options::ENABLE_STRIKETHROUGH);
-        options
-    });
+static CMARK_OPTIONS: Lazy<pulldown_cmark::Options> = Lazy::new(|| {
+    let mut options = pulldown_cmark::Options::empty();
+    options.insert(pulldown_cmark::Options::ENABLE_TABLES);
+    options.insert(pulldown_cmark::Options::ENABLE_STRIKETHROUGH);
+    options
+});
 
 #[pyfunction]
 fn markdownify(text: &str) -> String {
     let parser = pulldown_cmark::Parser::new_ext(text, *CMARK_OPTIONS);
-    let mut html_output = String::new();
-    pulldown_cmark::html::push_html(&mut html_output, parser);
-    AMMONIA.clean(&html_output).to_string()
+    let mut html = String::new();
+    pulldown_cmark::html::push_html(&mut html, parser);
+    AMMONIA.clean(&html).to_string()
 }
 
 #[pymodule]
