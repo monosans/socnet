a9af635059864478db28267b30223ffa327620ce
monosans <hsyqixco@protonmail.com>
2023-06-02 15:22:34 +0300

    Fix date formatter in messenger

--- socnet/static/private/ts/date_formatter.ts
+++ socnet/static/private/ts/date_formatter.ts
@@ -3,23 +3,22 @@ const options = { locale: navigator.language === "ru" ? "ru" : "en" };
 void (async function formatDates(): Promise<void> {
   const elements = document.querySelectorAll<HTMLElement>("[data-epoch]");
 
-  if (!elements.length) {
-    return;
+  if (elements.length) {
+    const { DateTime } = await import(
+      "https://cdn.jsdelivr.net/npm/luxon@3/+esm"
+    );
+    const relativeOptions = { base: DateTime.now() };
+    for (const el of elements) {
+      const dt = DateTime.fromSeconds(parseInt(el.dataset["epoch"]!), options);
+      const relativeDt = dt.toRelative(relativeOptions);
+      if (el.textContent !== relativeDt) {
+        el.textContent = relativeDt;
+      }
+      if (!el.title) {
+        el.title = dt.toLocaleString(DateTime.DATETIME_FULL);
+      }
+    }
   }
 
-  const { DateTime } = await import(
-    "https://cdn.jsdelivr.net/npm/luxon@3/+esm"
-  );
-  const relativeOptions = { base: DateTime.now() };
-  for (const el of elements) {
-    const dt = DateTime.fromSeconds(parseInt(el.dataset["epoch"]!), options);
-    const relativeDt = dt.toRelative(relativeOptions);
-    if (el.textContent !== relativeDt) {
-      el.textContent = relativeDt;
-    }
-    if (!el.title) {
-      el.title = dt.toLocaleString(DateTime.DATETIME_FULL);
-    }
-  }
   setTimeout(formatDates, 1000);
 })();
