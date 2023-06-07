b1f5527957011926a713736b7585f1629bd98585
monosans <hsyqixco@protonmail.com>
2023-06-07 10:34:59 +0300

    Make start.sh more readable

--- docker/django/start.sh
+++ docker/django/start.sh
@@ -7,7 +7,34 @@ python3 /app/manage.py collectstatic --noinput --clear
 python3 /app/manage.py compilemessages
 
 find /var/www/django/static -type f \
-	! -regex '^.+\.\(jpg\|jpeg\|png\|gif\|webp\|zip\|gz\|tgz\|bz2\|tbz\|xz\|br\|swf\|flv\|woff\|woff2\|3gp\|3gpp\|asf\|avi\|m4v\|mov\|mp4\|mpeg\|mpg\|webm\|wmv\)$' \
+	! \( \
+	-iname '*.jpg' -o \
+	-iname '*.jpeg' -o \
+	-iname '*.png' -o \
+	-iname '*.gif' -o \
+	-iname '*.webp' -o \
+	-iname '*.zip' -o \
+	-iname '*.gz' -o \
+	-iname '*.tgz' -o \
+	-iname '*.bz2' -o \
+	-iname '*.tbz' -o \
+	-iname '*.xz' -o \
+	-iname '*.br' -o \
+	-iname '*.swf' -o \
+	-iname '*.flv' -o \
+	-iname '*.woff' -o \
+	-iname '*.woff2' -o \
+	-iname '*.3gp' -o \
+	-iname '*.3gpp' -o \
+	-iname '*.asf' -o \
+	-iname '*.avi' -o \
+	-iname '*.m4v' -o \
+	-iname '*.mov' -o \
+	-iname '*.mp4' -o \
+	-iname '*.mpeg' -o \
+	-iname '*.mpg' -o \
+	-iname '*.webm' -o \
+	-iname '*.wmv' \) \
 	-exec brotli --force --best {} \+ \
 	-exec gzip --force --keep --best {} \+
 
