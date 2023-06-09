94c76a71ef3f2db7c211a06cf2b6c3225196bd60
monosans <hsyqixco@protonmail.com>
2023-06-09 23:36:21 +0300

    Preserve GET params in pagination

---
+++ socnet/core/templatetags/query_transform.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from typing import Literal, Protocol
+
+from django import template
+from django.http import HttpRequest
+
+register = template.Library()
+
+
+class Context(Protocol):
+    def __getitem__(self, __key: Literal["request"]) -> HttpRequest:
+        ...
+
+
+@register.simple_tag(takes_context=True)
+def query_transform(context: Context, **kwargs: str) -> str:
+    params = context["request"].GET.copy()
+    for k, v in kwargs.items():
+        params[k] = v
+    return params.urlencode()
