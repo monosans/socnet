f24818d2f9044b7c2338edb6992eb246418a2caf
monosans <hsyqixco@protonmail.com>
2023-08-24 13:09:30 +0300

    Add socnet_rs.markdownify tests

---
+++ tests/socnet_rs/test_markdownify.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+import pytest
+
+from socnet_rs import markdownify
+
+
+@pytest.mark.parametrize(
+    ("given", "expected"),
+    [
+        ("# Header", "<h1>Header</h1>\n"),
+        (
+            '<p>Paragraph</p><script>alert("Dangerous code")</script>',
+            "<p>Paragraph</p>",
+        ),
+    ],
+)
+def test_markdownify(given: str, expected: str) -> None:
+    assert markdownify(given) == expected
+
+
+def test_type_error() -> None:
+    exc_str = "argument 'value': 'int' object cannot be converted to 'PyString'"
+    with pytest.raises(TypeError, match=exc_str):
+        markdownify(0)  # type: ignore[arg-type]
