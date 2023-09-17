695d8cb80bc68e3118d6759df66576960b1603c0
monosans <hsyqixco@protonmail.com>
2023-09-17 11:50:30 +0300

    Add start.debugpy.sh

---
+++ docker/django/start.debugpy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+exec /usr/local/bin/python3 -m debugpy --wait-for-client --listen 0.0.0.0:5678 /usr/local/bin/uvicorn --host 0.0.0.0 --reload --reload-include '*.html' --log-config config/settings/logging.json --no-server-header config.asgi:application
