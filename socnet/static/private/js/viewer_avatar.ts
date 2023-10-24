b4286659cf5a1838240581287a218a93aafc1e7c
monosans <hsyqixco@protonmail.com>
2023-10-24 10:24:17 +0300

    Fix eslint errors

--- socnet/static/private/js/viewer_avatar.ts
+++ socnet/static/private/js/viewer_avatar.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/naming-convention
 import Viewer from "https://cdn.jsdelivr.net/npm/viewerjs@1/+esm";
 
 new Viewer(document.querySelector("#avatar")!, {
--- socnet/static/private/js/viewer_markdown.ts
+++ socnet/static/private/js/viewer_markdown.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/naming-convention
 import Viewer from "https://cdn.jsdelivr.net/npm/viewerjs@1/+esm";
 
 for (const container of document.querySelectorAll<HTMLElement>(
