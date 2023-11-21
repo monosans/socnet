8a5c4b4c50176697220240468d22893ed74734ba
monosans <hsyqixco@protonmail.com>
2023-11-21 15:35:41 +0300

    Remove unused eslint ignore

--- socnet/static/private/js/viewer_avatar.ts
+++ socnet/static/private/js/viewer_avatar.ts
@@ -1,4 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/naming-convention
 import Viewer from "https://cdn.jsdelivr.net/npm/viewerjs@1/+esm";
 
 new Viewer(document.querySelector("#avatar")!, {
--- socnet/static/private/js/viewer_markdown.ts
+++ socnet/static/private/js/viewer_markdown.ts
@@ -1,4 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/naming-convention
 import Viewer from "https://cdn.jsdelivr.net/npm/viewerjs@1/+esm";
 
 for (const container of document.querySelectorAll<HTMLElement>(
