2d6c4d76223d2210dcd7ab44ab9d8767ee8ebee2
monosans <hsyqixco@protonmail.com>
2023-12-26 15:00:08 +0300

    socnet_rs: explicitly specify crate

--- socnet_rs/src/lib.rs
+++ socnet_rs/src/lib.rs
@@ -6,8 +6,8 @@ use pyo3::prelude::*;
 
 #[pymodule]
 fn socnet_rs(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(markdownify::markdownify, m)?)?;
+    m.add_function(wrap_pyfunction!(crate::markdownify::markdownify, m)?)?;
     m.add_function(wrap_pyfunction!(crate::minify_html::minify_html, m)?)?;
-    m.add_function(wrap_pyfunction!(normalize_str::normalize_str, m)?)?;
+    m.add_function(wrap_pyfunction!(crate::normalize_str::normalize_str, m)?)?;
     Ok(())
 }
