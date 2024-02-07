af5d0869561f879e56d5e17cdfb1f46a7f46b2ae
monosans <hsyqixco@protonmail.com>
2024-02-07 12:51:28 +0300

    Fix mypy

--- socnet/api/permissions.py
+++ socnet/api/permissions.py
@@ -1,14 +1,11 @@
 from __future__ import annotations
 
 from copy import deepcopy
-from typing import ClassVar
 
 from rest_framework.permissions import DjangoModelPermissions
 
 
 class ActualDjangoModelPermissions(DjangoModelPermissions):
-    perms_map: ClassVar[dict[str, list[str]]] = deepcopy(
-        DjangoModelPermissions.perms_map
-    )
+    perms_map = deepcopy(DjangoModelPermissions.perms_map)
     perms_map["GET"] = ["%(app_label)s.view_%(model_name)s"]  # noqa: RUF012
     perms_map["HEAD"] = perms_map["GET"].copy()
