4859f5aa1bcf7a8b529c5d49dc2a6a9843569f48
monosans <hsyqixco@protonmail.com>
2024-03-27 10:24:33 +0300

    Remove unused argument

--- socnet_rs/src/lib.rs
+++ socnet_rs/src/lib.rs
@@ -5,7 +5,7 @@ mod normalize_str;
 use pyo3::prelude::*;
 
 #[pymodule]
-fn socnet_rs(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn socnet_rs(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(crate::markdownify::markdownify, m)?)?;
     m.add_function(wrap_pyfunction!(crate::minify_html::minify_html, m)?)?;
     m.add_function(wrap_pyfunction!(crate::normalize_str::normalize_str, m)?)?;
