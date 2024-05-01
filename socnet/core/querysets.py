0f0962e73ff2b65b3f4cc368612587e0ddbe0b14
monosans <hsyqixco@protonmail.com>
2024-05-01 12:11:12 +0500

    Fix type ignore

--- socnet/core/querysets.py
+++ socnet/core/querysets.py
@@ -2,10 +2,10 @@ from __future__ import annotations
 
 from django.db.models import QuerySet
 from django.db.models.functions import Extract
-from typing_extensions import Self
+from typing_extensions import Any, Self
 
 
-class TimestampedModelQuerySet(QuerySet):  # type: ignore[type-arg]
+class TimestampedModelQuerySet(QuerySet[Any]):
     def annotate_epoch_dates(self) -> Self:
         return self.annotate(
             date_created_epoch=Extract("date_created", "epoch"),
