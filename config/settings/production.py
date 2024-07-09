4ce16b3aad48da3b8dee9f6ae5c480248917212b
monosans <hsyqixco@protonmail.com>
2024-07-09 16:57:12 +0300

    Reconfigure sentry

--- config/settings/production.py
+++ config/settings/production.py
@@ -52,12 +52,12 @@ STORAGES = {
 _SENTRY_DSN = env.str("SENTRY_DSN", None)
 if _SENTRY_DSN:
     import sentry_sdk
-    from sentry_sdk.integrations.django import DjangoIntegration
-    from sentry_sdk.integrations.redis import RedisIntegration
+    from sentry_sdk.integrations.asyncio import AsyncioIntegration
 
     sentry_sdk.init(
         dsn=_SENTRY_DSN,
-        integrations=(DjangoIntegration(), RedisIntegration()),
+        integrations=(AsyncioIntegration(),),
+        max_request_body_size="always",
         traces_sample_rate=env.float("SENTRY_TRACES_SAMPLE_RATE", 0.0),
     )
 else:
