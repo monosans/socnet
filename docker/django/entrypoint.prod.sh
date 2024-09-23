98854b1ebb74a28b10a3d08c32b2183576fe6379
monosans <hsyqixco@protonmail.com>
2024-09-23 10:52:02 +0300

    wait for postgres and redis concurrently

--- docker/django/entrypoint.prod.sh
+++ docker/django/entrypoint.prod.sh
@@ -2,7 +2,8 @@
 
 set -euo pipefail
 
-wait-for-it "${POSTGRES_HOST}:${POSTGRES_PORT}"
-wait-for-it "${REDIS_HOST}:${REDIS_PORT}"
+wait-for-it "${POSTGRES_HOST}:${POSTGRES_PORT}" &
+wait-for-it "${REDIS_HOST}:${REDIS_PORT}" &
+wait
 
 exec "$@"
