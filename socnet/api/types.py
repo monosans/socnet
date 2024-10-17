8d9ff5a83c3a7150d67eb23fa7aec185c7e34976
monosans <hsyqixco@protonmail.com>
2024-10-17 14:35:15 +0300

    fix: don't use new typealias syntax

--- socnet/api/types.py
+++ socnet/api/types.py
@@ -4,14 +4,14 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from collections.abc import Sequence
-    from typing import Literal
+    from typing import Literal, TypeAlias
 
     from rest_framework.request import Request
 
     from ..users.models import User
 
-    type SerializerFields = Sequence[str] | Literal["__all__"] | None
-    type SerializerExclude = Sequence[str] | None
+    SerializerFields: TypeAlias = Sequence[str] | Literal["__all__"] | None
+    SerializerExclude: TypeAlias = Sequence[str] | None
 
     class AuthedRequest(Request):
         user: User
