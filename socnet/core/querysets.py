6534bd9bc738e1aa5dbc316c66d9e6db20702ffc
monosans <hsyqixco@protonmail.com>
2024-10-28 08:54:01 +0300

    chore: fix TimestampedModelQuerySet type hint

--- socnet/core/querysets.py
+++ socnet/core/querysets.py
@@ -6,16 +6,17 @@ from django.db.models import QuerySet
 from django.db.models.functions import Extract
 
 if TYPE_CHECKING:
-    from django.db.models import Model
     from django_stubs_ext import WithAnnotations
     from typing_extensions import TypedDict
+
+    from .models import TimestampedModel
 
     class EpochDates(TypedDict):
         date_created_epoch: int
         date_updated_epoch: int
 
 
-class TimestampedModelQuerySet[T: Model](QuerySet[T]):
+class TimestampedModelQuerySet[T: TimestampedModel](QuerySet[T]):
     def annotate_epoch_dates(
         self,
     ) -> TimestampedModelQuerySet[WithAnnotations[T, EpochDates]]:
