a7dbfa8852afb764e2c604ebd90d510704151a80
monosans <hsyqixco@protonmail.com>
2024-11-05 10:00:37 +0300

    chore: remove .djlintrc from prettier config

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -2,7 +2,6 @@
  * @type {import('prettier').Config & import("@ianvs/prettier-plugin-sort-imports").PluginConfig}
  */
 export default {
-  overrides: [{ files: [".djlintrc"], options: { parser: "json" } }],
   plugins: [
     "@ianvs/prettier-plugin-sort-imports",
     "prettier-plugin-packagejson",
