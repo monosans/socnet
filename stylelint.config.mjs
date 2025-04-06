39e655ddbcb59457fdd304aa13e23c7736fc6e8b
monosans <hsyqixco@protonmail.com>
2025-04-06 19:08:10 +0300

    update stylelint

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -3,5 +3,4 @@
 /** @type {import('stylelint').Config} */
 export default {
   extends: ["stylelint-config-standard", "stylelint-config-recess-order"],
-  rules: { "color-function-notation": "legacy" },
 };
