3955275fa95a099a1de7314b0c76e428ef1a3c93
monosans <hsyqixco@protonmail.com>
2025-04-06 20:45:35 +0300

    fix eslint

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-
 /** @type {import('stylelint').Config} */
 export default {
   extends: ["stylelint-config-standard", "stylelint-config-recess-order"],
