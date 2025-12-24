fe3e3acad5a41ddbd2a4f73cb58fbdb1d381451d
monosans <hsyqixco@protonmail.com>
2025-12-24 13:32:48 +0300

    perf(socnet-rs): use compact_str

--- socnet_rs/src/normalize_str.rs
+++ socnet_rs/src/normalize_str.rs
@@ -1,19 +1,32 @@
-use itertools::Itertools as _;
 use pyo3::prelude::*;
 
 #[pyfunction]
 #[pyo3(signature = (value, /))]
-pub fn normalize_str(py: Python<'_>, value: &str) -> String {
-    py.detach(move || {
-        value
-            .lines()
-            .filter_map(|line| {
-                if line.trim().is_empty() {
-                    None
+pub fn normalize_str<'py>(
+    py: Python<'py>,
+    value: &str,
+) -> Bound<'py, pyo3::types::PyString> {
+    let result = py.detach(move || {
+        let mut out = compact_str::CompactString::const_new("");
+        let mut first_line = true;
+
+        for line in value.lines() {
+            let mut words = line.split_whitespace();
+            if let Some(first_word) = words.next() {
+                if first_line {
+                    first_line = false;
                 } else {
-                    Some(line.split_whitespace().join(" "))
+                    out.push('\n');
                 }
-            })
-            .join("\n")
-    })
+                out.push_str(first_word);
+                for w in words {
+                    out.push(' ');
+                    out.push_str(w);
+                }
+            }
+        }
+
+        out
+    });
+    pyo3::types::PyString::new(py, &result)
 }
