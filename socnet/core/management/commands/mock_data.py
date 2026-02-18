e118e7a5c8caa0ab8b9686c9317534563a0b9866
monosans <hsyqixco@protonmail.com>
2026-02-18 16:24:27 +0300

    fix: improve mock data generator

--- socnet/core/management/commands/mock_data.py
+++ socnet/core/management/commands/mock_data.py
@@ -17,24 +17,24 @@ if TYPE_CHECKING:
 class Command(BaseCommand):
     @override
     def add_arguments(self, parser: CommandParser) -> None:
-        parser.add_argument("--users", type=int, default=10000)
-        parser.add_argument("--posts-per-user", type=int, default=10)
-        parser.add_argument("--comments-per-post", type=int, default=10)
-        parser.add_argument("--subscriptions-per-user", type=int, default=10)
-        parser.add_argument("--likes-per-post", type=int, default=10)
-        parser.add_argument("--likes-per-comment", type=int, default=10)
-        parser.add_argument("--messages-per-user", type=int, default=10)
+        parser.add_argument("--users", type=int, default=5000)
+        parser.add_argument("--posts", type=int, default=1000000)
+        parser.add_argument("--comments", type=int, default=1000000)
+        parser.add_argument("--messages", type=int, default=100000)
+        parser.add_argument("--subscriptions", type=int, default=100000)
+        parser.add_argument("--post-likes", type=int, default=100000)
+        parser.add_argument("--comment-likes", type=int, default=5000000)
         parser.add_argument("--purge", action="store_true")
 
     @override
     def handle(self, *args: Any, **options: Any) -> None:
         users_count: int = options["users"]
-        posts_per_user: int = options["posts_per_user"]
-        comments_per_post: int = options["comments_per_post"]
-        messages_per_user: int = options["messages_per_user"]
-        subscriptions_per_user: int = options["subscriptions_per_user"]
-        likes_per_post: int = options["likes_per_post"]
-        likes_per_comment: int = options["likes_per_comment"]
+        posts_count: int = options["posts"]
+        comments_count: int = options["comments"]
+        messages_count: int = options["messages"]
+        subscriptions_count: int = options["subscriptions"]
+        post_likes_count: int = options["post_likes"]
+        comment_likes_count: int = options["comment_likes"]
         purge: bool = options["purge"]
 
         from socnet.blog.models import Comment, Post
@@ -46,7 +46,7 @@ class Command(BaseCommand):
 
         with transaction.atomic():
             if purge:
-                self.stdout.write("Purging existing data...\n")
+                self.stdout.write("Purging existing data...\n\n")
                 Comment.objects.all().delete()
                 Post.objects.all().delete()
                 Message.objects.all().delete()
@@ -58,88 +58,95 @@ class Command(BaseCommand):
                 UserFactory.build() for _ in range(users_count)
             )
             self.stdout.write(
-                f"Created {len(users)} users"
-                f" in {perf_counter() - started:,.0f} seconds."
+                f"Created {len(users):,} users"
+                f" in {perf_counter() - started:,.2f} seconds."
             )
 
             self.stdout.write("\nCreating posts...")
             started = perf_counter()
             posts = Post.objects.bulk_create(
-                PostFactory.build(author=u)
-                for u in users
-                for _ in range(random.randint(0, posts_per_user))
+                PostFactory.build(author=random.choice(users))
+                for _ in range(posts_count)
             )
             self.stdout.write(
-                f"Created {len(posts)} posts"
-                f" in {perf_counter() - started:,.0f} seconds."
+                f"Created {len(posts):,} posts"
+                f" in {perf_counter() - started:,.2f} seconds."
             )
 
             self.stdout.write("\nCreating comments...")
             started = perf_counter()
             comments = Comment.objects.bulk_create(
-                CommentFactory.build(post=p, author=random.choice(users))
-                for p in posts
-                for _ in range(random.randint(0, comments_per_post))
+                CommentFactory.build(
+                    post=random.choice(posts), author=random.choice(users)
+                )
+                for _ in range(comments_count)
             )
             self.stdout.write(
-                f"Created {len(comments)} comments"
-                f" in {perf_counter() - started:,.0f} seconds."
+                f"Created {len(comments):,} comments"
+                f" in {perf_counter() - started:,.2f} seconds."
             )
 
             self.stdout.write("\nCreating messages...")
             started = perf_counter()
             messages = Message.objects.bulk_create(
                 MessageFactory.build(
-                    sender=u,
+                    sender=(u := random.choice(users)),
                     recipient=random.choice([x for x in users if x.pk != u.pk]),
                 )
-                for u in users
-                for _ in range(random.randint(0, messages_per_user))
+                for _ in range(messages_count)
             )
             self.stdout.write(
-                f"Created {len(messages)} messages"
-                f" in {perf_counter() - started:,.0f} seconds."
+                f"Created {len(messages):,} messages"
+                f" in {perf_counter() - started:,.2f} seconds."
             )
 
             self.stdout.write("\nCreating subscriptions...")
             started = perf_counter()
             subscriptions = User.subscriptions.through.objects.bulk_create(
-                User.subscriptions.through(
-                    from_user_id=from_u.pk, to_user_id=to_u.pk
-                )
-                for from_u in users
-                for to_u in random.sample(
-                    [x for x in users if x.pk != from_u.pk],
-                    random.randint(0, min(len(users), subscriptions_per_user)),
-                )
+                (
+                    User.subscriptions.through(
+                        from_user_id=(u := random.choice(users)).pk,
+                        to_user_id=random.choice([
+                            x for x in users if x.pk != u.pk
+                        ]).pk,
+                    )
+                    for _ in range(subscriptions_count)
+                ),
+                ignore_conflicts=True,
             )
             self.stdout.write(
-                f"Created {len(subscriptions)} subscriptions"
-                f" in {perf_counter() - started:,.0f} seconds."
+                f"Created {len(subscriptions):,} subscriptions"
+                f" in {perf_counter() - started:,.2f} seconds."
             )
             self.stdout.write("\nCreating post likes...")
             post_likers = Post.likers.through.objects.bulk_create(
-                Post.likers.through(post_id=p.pk, user_id=u.pk)
-                for p in posts
-                for u in random.sample(
-                    users, random.randint(0, min(len(users), likes_per_post))
-                )
+                (
+                    Post.likers.through(
+                        post_id=random.choice(posts).pk,
+                        user_id=random.choice(users).pk,
+                    )
+                    for _ in range(post_likes_count)
+                ),
+                ignore_conflicts=True,
             )
             self.stdout.write(
-                f"Created {len(post_likers)} post likes"
-                f" in {perf_counter() - started:,.0f} seconds."
+                f"Created {len(post_likers):,} post likes"
+                f" in {perf_counter() - started:,.2f} seconds."
             )
 
             self.stdout.write("\nCreating comment likes...")
             started = perf_counter()
             comment_likers = Comment.likers.through.objects.bulk_create(
-                Comment.likers.through(comment_id=c.pk, user_id=u.pk)
-                for c in comments
-                for u in random.sample(
-                    users, random.randint(0, min(len(users), likes_per_comment))
-                )
+                (
+                    Comment.likers.through(
+                        comment_id=random.choice(comments).pk,
+                        user_id=random.choice(users).pk,
+                    )
+                    for _ in range(comment_likes_count)
+                ),
+                ignore_conflicts=True,
             )
             self.stdout.write(
-                f"Created {len(comment_likers)} comment likes"
-                f" in {perf_counter() - started:,.0f} seconds."
+                f"Created {len(comment_likers):,} comment likes"
+                f" in {perf_counter() - started:,.2f} seconds."
             )
